8effddd6e418097235aeeff1a21ad21abbab9595
CEEXAGON <acejayodom@gmail.com>
2022-03-15 14:06:34 +0700

    save

--- Photo and Progress.py
+++ Photo and Progress.py
@@ -7,12 +7,12 @@ root.title("pictures and shits")
 myImg1 = ImageTk.PhotoImage(Image.open("get-real.gif"))
 myImg2 = ImageTk.PhotoImage(Image.open("GOJO.jpg"))
 imgList = (myImg1,myImg2)
-myLabel = Label(image = myImg1)
-myLabel.pack()
+myLabel = Label(image = myImg1).grid(row=0,column=0,columnspan=3)
 
-buttonQuit = Button(root,text = "exit program",command=root.quit)
-buttonQuit.pack()
-status = Label(root,text="progress:1 of"+str(len(imgList)))
-status.pack()
+buttonQuit = Button(root,text = "exit program",command=root.quit).grid(row=1,column=1)
+buttonLeft = Button(root,text = "<<").grid(row=1,column=0)
+buttonRight = Button(root,text = ">>").grid(row=1,column=2)
+status = Label(root,text="progress: of"+str(len(imgList)))
+
 
 root.mainloop()
